1484b88af74405dc8782be19bf56bf6caf2c4a99
pageweb-2024 <152339341+pageweb-2023@users.noreply.github.com>
2025-10-01 16:17:59 -0500

    nuevos

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
-window.addEventListener("DOMContentLoaded", () => {
-    if (!document.getElementById("musicaFondo")) {
-        let audio = document.createElement("audio");
-        audio.id = "musicaFondo";
-        audio.src = "musica/fondo.mp3"; // ruta desde index
-        audio.autoplay = true;
-        audio.loop = true;
-        audio.volume = 0.4; // volumen más bajo
-        document.body.appendChild(audio);
+document.getElementById("buscador").addEventListener("submit", function(e) {
+    e.preventDefault(); // Evita que se recargue la página
+  
+    let query = document.getElementById("searchInput").value.toLowerCase();
+  
+    if (query.includes("fruta")) {
+      window.location.href = "sections/frutas.html"; 
+    } else if (query.includes("verdura")) {
+      window.location.href = "sections/verduras.html"; 
+    } else if (query.includes("grano")){
+        window.location.href= "sections/granos.html";
+    }else if (query.includes("carrito")) {
+      window.location.href = "sections/carrito.html"; 
+    } else {
+      alert("No se encontró la categoría: " + query);
     }
-});
+  });
+  
